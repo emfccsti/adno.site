51983e1399ec5cee11fff85079026c2451d775aa
Thierry Pasquier <thierry.pasquier@emf.ccsti.eu>
2022-06-26 10:52:02 +0200

    Script de déploiement

---
+++ deployment.sh
@@ -0,0 +1,14 @@
+# Build static pages
+hugo 
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m "Publication de Adno"
+
+# Push source and build repos.
+git push origin main
